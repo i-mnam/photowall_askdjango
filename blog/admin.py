420a94d15e245ab916c8cf1c13ff5fe20bde567c
i-mnam <8linkbv@gmail.com>
2019-02-01 23:24:13 +0900

    blog.models.Post 모델을 등록하고, admin 연결

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post) # admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'is_public', 'created_at']
+    list_display_links = ['title']
+    list_editable = ['is_public']
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
+# blog/models.py
+
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    is_public = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
