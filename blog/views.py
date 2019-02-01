fd1558fc7bb1736fb3afd65b5946a2a6836cedc8
i-mnam <8linkbv@gmail.com>
2019-02-02 00:24:43 +0900

    blog.views.index //post_list 구현

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def index(request):
+    
+    post_list = Post.objects.all()
+    print('hello',post_list)
+    return render(request, 'blog/post_list.html', {
+        'post_list':post_list,
+    })
