a689905aec3c75d19f9bc3f205ad8a2f413f59f0
i-mnam <8linkbv@gmail.com>
2019-02-02 13:33:20 +0900

    blog.views.post_detail 뷰 구현

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
 ]
