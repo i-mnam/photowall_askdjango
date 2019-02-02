0157242d40ca0ae785d3d9c2a2b1c521486584fe
i-mnam <8linkbv@gmail.com>
2019-02-02 15:44:56 +0900

    blog.models.Post 모델에 lnglat 필드를 추가하고, 입력된 경도/위도에 맞춰 구글맵 노출

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,37 @@
 # blog/models.py
 
+import re
+from django import forms
 from django.db import models
 
 
+def lnglat_validator(value):
+    if not re.match(r'^[+-]?\d+\.?\d*,[+-]?\d+\.?\d*$', value):
+        raise forms.ValidationError('경도,위도를 입력해주세요.')
+
+
 class Post(models.Model):
+    print("[class Post 호출]") # server on..
     title = models.CharField(max_length=100)
     content = models.TextField()
     photo = models.ImageField(default='') #no옵션 필수필드 #경로만 저장
+    lnglat = models.CharField(max_length=40, blank=True, validators=[lnglat_validator]) 
+    # PointField GeoDjango 
     is_public = models.BooleanField(default=False) # 공개여부
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+    @property
+    def lng(self):
+        if self.lnglat:
+            return self.lnglat.split(',')[0]
+        return None
+    
+    @property
+    def lat(self):
+        if self.lnglat:
+            return self.lnglat.split(',')[1]
+        return None
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,6 @@ from .models import Post
 
 def index(request):
     post_list = Post.objects.all()
-    print('hello',post_list)
     return render(request, 'blog/post_list.html', {
         'post_list':post_list,
     })
